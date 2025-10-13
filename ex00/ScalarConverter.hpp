44bccc056eee2c1dfef320c6e41e9480498dcb84
ricvrdv <costagarcia.r95@gmail.com>
2025-10-13 14:14:55 +0100

    feat(ex00): add main and ScalarConverter class

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjesus-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 13:55:36 by rjesus-d          #+#    #+#             */
+/*   Updated: 2025/10/13 13:55:42 by rjesus-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <iostream>
+
+class   ScalarConverter
+{
+    private:
+
+        ScalarConverter();
+        ScalarConverter( ScalarConverter const &other );
+        ScalarConverter &operator
+        ~ScalarConverter();
+
+
+    public:
+};
+
+#endif
